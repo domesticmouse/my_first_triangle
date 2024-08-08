3fc62229104678e7620aa8d3e954e7078df3b65f
Brett Morgan <brettmorgan@google.com>
2024-08-08 13:19:02 +1000

    Add shaders

---
+++ hook/build.dart
@@ -0,0 +1,18 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:native_assets_cli/native_assets_cli.dart';
+import 'package:flutter_gpu_shaders/build.dart';
+
+void main(List<String> args) async {
+  await build(
+    args,
+    (config, output) async {
+      await buildShaderBundleJson(
+          buildConfig: config,
+          buildOutput: output,
+          manifestFileName: 'my_first_triangle.shaderbundle.json');
+    },
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,25 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_gpu/gpu.dart' as gpu;
+
+const String _kShaderBundlePath =
+    'build/shaderbundles/my_renderer.shaderbundle';
+
+gpu.ShaderLibrary? _shaderLibrary;
+
+gpu.ShaderLibrary get shaderLibrary {
+  _shaderLibrary ??= gpu.ShaderLibrary.fromAsset(_kShaderBundlePath);
+  if (_shaderLibrary == null) {
+    throw Exception('Failed to load shader bundle');
+  }
+
+  return _shaderLibrary!;
+}
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +28,111 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      title: 'Flutter GPU Triangle Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('Foreground'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextButton(
+              onPressed: () {},
+              child: const Text('Background'),
             ),
           ],
         ),
+        body: CustomPaint(
+          painter: TrianglePainter(),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class TrianglePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final texture = gpu.gpuContext.createTexture(
+        gpu.StorageMode.devicePrivate, size.width.ceil(), size.height.ceil());
+    if (texture == null) {
+      throw Exception('Failed to create texture');
+    }
+
+    final renderTarget = gpu.RenderTarget.singleColor(
+      gpu.ColorAttachment(
+        texture: texture,
+        clearValue: const Color.fromARGB(255, 54, 131, 169),
+        loadAction: gpu.LoadAction.clear,
+        storeAction: gpu.StoreAction.store,
+      ),
+    );
+
+    final commandBuffer = gpu.gpuContext.createCommandBuffer();
+    final renderPass = commandBuffer.createRenderPass(renderTarget);
+
+    final vert = shaderLibrary['SimpleVertex'];
+    if (vert == null) {
+      throw Exception('Failed to load SimpleVertex vertex shader');
+    }
+
+    final frag = shaderLibrary['SimpleFragment'];
+    if (frag == null) {
+      throw Exception('Failed to load SimpleFragment fragment shader');
+    }
+
+    final pipeline = gpu.gpuContext.createRenderPipeline(vert, frag);
+
+    final verticesDeviceBuffer = gpu.gpuContext.createDeviceBufferWithCopy(
+      ByteData.sublistView(
+        Float32List.fromList([
+          // x, y
+          -0.5, -0.5, // First vertex
+          0.5, -0.5, // Second vertex
+          0.0, 0.5, // Third vertex
+        ]),
+      ),
+    );
+    if (verticesDeviceBuffer == null) {
+      throw Exception('Failed to create device buffer');
+    }
+
+    final colorBuffer = gpu.gpuContext
+        .createDeviceBufferWithCopy(ByteData.sublistView(Float32List.fromList([
+      // r,g,b,a
+      0.8, 0.0, 0.8, 1.0,
+    ])));
+    if (colorBuffer == null) {
+      throw Exception('Failed to create color buffer');
+    }
+
+    renderPass.bindPipeline(pipeline);
+
+    final verticesView = gpu.BufferView(
+      verticesDeviceBuffer,
+      offsetInBytes: 0,
+      lengthInBytes: verticesDeviceBuffer.sizeInBytes,
+    );
+    renderPass.bindVertexBuffer(verticesView, 3);
+
+    final colorView = gpu.BufferView(
+      colorBuffer,
+      offsetInBytes: 0,
+      lengthInBytes: colorBuffer.sizeInBytes,
+    );
+    renderPass.bindUniform(frag.getUniformSlot('FragInfo'), colorView);
+
+    renderPass.draw();
+
+    commandBuffer.submit();
+    final image = texture.asImage();
+    canvas.drawImage(image, Offset.zero, Paint());
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
