eaad4d0af116853bb9495a00baf549d0483f2ee1
Brett Morgan <brettmorgan@google.com>
2024-08-09 14:45:30 +1000

    Fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_first_triangle/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Foud buttons smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      const MaterialApp(
+        title: 'Flutter GPU Triangle test',
+        home: MyApp(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our app has four text buttons
+    expect(find.text('Vert1'), findsOneWidget);
+    expect(find.text('Vert2'), findsOneWidget);
+    expect(find.text('Vert3'), findsOneWidget);
+    expect(find.text('Background'), findsOneWidget);
   });
 }
