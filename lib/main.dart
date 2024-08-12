ee8cb4f1a97c2b19cc973c99d493c1e1c0aa6449
Brett Morgan <brettmorgan@google.com>
2024-08-13 09:24:26 +1000

    Two triangles!

--- lib/main.dart
+++ lib/main.dart
@@ -127,8 +127,6 @@ class TrianglePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    debugPrint('Painting triangle, size: $size');
-
     final texture = gpu.gpuContext.createTexture(
         gpu.StorageMode.devicePrivate, size.width.ceil(), size.height.ceil());
     if (texture == null) {
@@ -158,24 +156,36 @@ class TrianglePainter extends CustomPainter {
     }
 
     final pipeline = gpu.gpuContext.createRenderPipeline(vert, frag);
+    const floatsPerVertex = 6;
+    final vertexList = [
+      // layout:
+      //   x, y
+      //   vertex color (r,g,b,a)
 
+      // Triangle #1
+      -0.5, -0.5, // First vertex
+      color1.red / 255, color1.green / 255, color1.blue / 255,
+      color1.alpha / 255, // vertex color
+      0.5, -0.5, // Second vertex
+      color2.red / 255, color2.green / 255, color2.blue / 255,
+      color2.alpha / 255, // vertex color
+      -0.5, 0.5, // Third vertex
+      color3.red / 255, color3.green / 255, color3.blue / 255,
+      color3.alpha / 255, // vertex color
+
+      // Triangle #2
+      0.5, -0.5, // Shared vertex
+      color2.red / 255, color2.green / 255, color2.blue / 255,
+      color2.alpha / 255, // vertex color
+      0.5, 0.5, // Not shared vertex
+      color1.red / 255, color1.green / 255, color1.blue / 255,
+      color1.alpha / 255, // vertex color
+      -0.5, 0.5, // Shared vertex
+      color3.red / 255, color3.green / 255, color3.blue / 255,
+      color3.alpha / 255, // vertex color
+    ];
     final verticesDeviceBuffer = gpu.gpuContext.createDeviceBufferWithCopy(
-      ByteData.sublistView(
-        Float32List.fromList([
-          // layout:
-          //   x, y
-          //   vertex color (r,g,b,a)
-          -0.5, -0.5, // First vertex
-          color1.red / 255, color1.green / 255, color1.blue / 255,
-          color1.alpha / 255, // vertex color
-          0.5, -0.5, // Second vertex
-          color2.red / 255, color2.green / 255, color2.blue / 255,
-          color2.alpha / 255, // vertex color
-          0.0, 0.5, // Third vertex
-          color3.red / 255, color3.green / 255, color3.blue / 255,
-          color3.alpha / 255, // vertex color
-        ]),
-      ),
+      ByteData.sublistView(Float32List.fromList(vertexList)),
     );
     if (verticesDeviceBuffer == null) {
       throw Exception('Failed to create device buffer');
@@ -188,7 +198,8 @@ class TrianglePainter extends CustomPainter {
       offsetInBytes: 0,
       lengthInBytes: verticesDeviceBuffer.sizeInBytes,
     );
-    renderPass.bindVertexBuffer(verticesView, 3);
+    renderPass.bindVertexBuffer(
+        verticesView, vertexList.length ~/ floatsPerVertex);
 
     renderPass.draw();
 
